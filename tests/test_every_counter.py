e0c6acc9f15edbc6a2afe11d973c44dadd1e7e63
asmolkin <asmolkin@ozon.ru>
2022-12-14 10:16:57 +0300

    add test

--- tests/test_every_counter.py
+++ tests/test_every_counter.py
@@ -1,10 +1,19 @@
+from collections import Counter
+
 from every_counter.every_counter import count_entries
 
 
-def every_counter_positive():
-    string = "Жди когда идут дожди"\
-             "Когда солнце в небе ясно"\
-            "Когда никто уже не ждет"\
-            "Ты жди, всем чертям назло"
+def test_every_counter_positive():
+    expected_result = Counter({'Когда': 2, 'Жди': 1,
+                       'когда': 1, 'идут': 1,
+                       'дожди': 1, 'солнце': 1,
+                       'в': 1, 'небе': 1, 'ясно': 1,
+                       'никто': 1, 'уже': 1, 'не': 1,
+                       'ждет': 1, 'Ты': 1, 'жди': 1,
+                       'всем': 1, 'чертям': 1, 'назло': 1})
+    string = "Жди когда идут дожди "\
+             "Когда солнце в небе ясно "\
+            "Когда никто уже не ждет "\
+            "Ты жди, всем чертям назло "
     counted_string = count_entries(string)
-    assert 1 == 1
+    assert expected_result == counted_string
